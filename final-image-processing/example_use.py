305581e55f7fddfbcbd02d5b3866177866c991f8
Mahatma Wisesa <56577748+hatmahat@users.noreply.github.com>
2021-08-21 14:09:30 +0700

    Add files via upload

--- final-image-processing/example_use.py
+++ final-image-processing/example_use.py
@@ -1,8 +1,9 @@
 import segmentimg as simg
 
-# Example use if segmentimg
+# Example use of segmentimg
 def with_sito():
-    img.hist_equal_all(on='rescaled_dict')
+    print("---- WITH SITO RUNNING ----")
+    img.hist_equal_all(on='org_dict')
     img.save_hist_equal_all(r"\backup2\histeq")
 
     img.bilateral_all(on='hist_equal_dict')
@@ -11,24 +12,27 @@ def with_sito():
     img.thresh_green_chan_all(on='bilateral_dict')
     img.save_thresh_green_chan_all(r"\backup2\thresh green")
 
-    img.masked_all(on='thresh_inv_green')
+    img.masked_all(on='thresh_inv_green', mask='org_dict')
     img.save_masked_all(r"\backup2\masked with sito")
+    print("---- WITH SITO DONE ----")
 
 def with_no_sito():
-    img.bilateral_all(on='rescaled_dict')
+    print("---- WITH NO SITO RUNNING ----")
+    img.bilateral_all(on='org_dict')
     img.save_bilateral_all(r"\backup2\bilateral no sito")
     
     img.thresh_gray_all(on='bilateral_dict')
     img.save_thresh_gray(r"\backup2\thresh gray")
 
-    img.masked_all(on='thresh_inv_gray')
+    img.masked_all(on='thresh_inv_gray', mask='org_dict')
     img.save_masked_all(r"\backup2\masked with no sito")
+    print("---- WITH NO SITO DONE ----")
 
-img = simg.segmentImg(r"C:\Users\Mahatma Ageng Wisesa\Desktop\OpenCV", "test")
+img = simg.segmentImg(r"C:\Users\Mahatma Ageng Wisesa\Desktop\OpenCV", "test2")
 img.read_all_img()
 
-img.rescale_all()
-img.save_rescaled_all(r"\backup2\rescale")
+# img.rescale_all()
+# img.save_rescaled_all(r"\backup2\rescale")
 
 with_sito()
 with_no_sito()
